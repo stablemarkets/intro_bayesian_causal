7f4db4b8142e9095bcff3a13df053e0eda7eb3c4
Arman Oganisian <arman.oganisian@gmail.com>
2020-02-24 12:18:32 -0500

    Added Nonparametric Bayes model (BART, DP, GP) implementation.

---
+++ Nonparametrics/gaussian_process_with_HPs.stan
@@ -0,0 +1,90 @@
+functions {
+  
+  // generate draw of function from GP posterior
+  vector gp_pred_rng(real[] x2, vector y1, real[] x1, real alpha,
+                     real rho, real sigma, real delta) {
+    int N1 = rows(y1);
+    int N2 = size(x2);
+    vector[N2] f2;
+    
+    {
+      matrix[N1, N1] L_K;
+      vector[N1] K_div_y1;
+      matrix[N1, N2] k_x1_x2;
+      matrix[N1, N2] v_pred;
+      vector[N2] f2_mu;
+      matrix[N2, N2] cov_f2;
+      matrix[N2, N2] diag_delta;
+      matrix[N1, N1] K;
+      
+      K = cov_exp_quad(x1, alpha, rho);
+      
+      for (n in 1:N1)
+        K[n, n] = K[n,n] + square(sigma);
+        
+      L_K = cholesky_decompose(K);
+      K_div_y1 = mdivide_left_tri_low(L_K, y1);
+      K_div_y1 = mdivide_right_tri_low(K_div_y1', L_K)';
+      k_x1_x2 = cov_exp_quad(x1, x2, alpha, rho);
+      f2_mu = (k_x1_x2' * K_div_y1);
+      v_pred = mdivide_left_tri_low(L_K, k_x1_x2);
+      cov_f2 = cov_exp_quad(x2, alpha, rho) - v_pred' * v_pred;
+      diag_delta = diag_matrix(rep_vector(delta, N2));
+
+      f2 = multi_normal_rng(f2_mu, cov_f2 + diag_delta);
+    }
+    return f2;
+  }
+}
+
+data {
+  int<lower=1> N1; // training data sample size
+
+  // training data, could set these to be row vectors for several variables
+  real x1[N1]; 
+  vector[N1] y1; // training outcome
+
+  int<lower=1> N2; // test data sample size
+  real x2[N2]; // test data 
+}
+
+transformed data {
+  vector[N1] mu = rep_vector(0, N1);
+  real delta = 1e-9;
+}
+
+parameters {
+  // inference for hyperparameters
+  real<lower=0> rho;
+  real<lower=0> alpha;
+  real<lower=0> sigma;
+}
+
+model {
+  matrix[N1, N1] L_K;
+
+  {
+    matrix[N1, N1] K = cov_exp_quad(x1, alpha, rho);
+    real sq_sigma = square(sigma);
+
+    // diagonal elements
+    for (n1 in 1:N1)
+      K[n1, n1] = K[n1, n1] + sq_sigma;
+
+    L_K = cholesky_decompose(K);
+  }
+
+  rho ~ inv_gamma(1, 1);
+  alpha ~ normal(0, 1);
+  sigma ~ normal(0, 3 );
+
+  y1 ~ multi_normal_cholesky(mu, L_K);
+}
+generated quantities {
+  vector[N2] f_test;
+  vector[N1] f_train;
+
+  f_test = gp_pred_rng(x2, y1, x1, alpha, rho, sigma, delta);
+  f_train = gp_pred_rng(x1, y1, x1, alpha, rho, sigma, delta);
+
+}
---
+++ Nonparametrics/npbayes.R
@@ -0,0 +1,162 @@
+install.packages('fdm2id',dependencies = T)
+library(fdm2id)
+library(ChiRP)
+library(BayesTree)
+library(rstan)
+library(gtools)
+
+#### ------------------------ Simulate Data --------------------------------####
+set.seed(1)
+
+N = 500
+X = seq(0, 2*pi, length.out = N)
+
+X_01 = ((X - min(X)) / ( max(X) - min(X) ))
+
+oscil1 = exp(-1*X)*cos(2*pi*X)
+oscil2 = rev((exp(-1*X)*cos(2*pi*X)))
+
+Y = (X_01)*oscil1 + (rev(X_01))*oscil2  + rnorm(N,0, .02)
+
+test = ifelse(X>2.5 & X<3.75, 1, 0)
+
+
+plot(X, Y, col=ifelse(test==1, 'red', 'blue'), pch=20)
+
+d = data.frame(X=scale(X), Y=scale(Y))
+
+#### ------------------------ Dirichlet Process ----------------------------####
+
+d_train = d[test==0, ]
+d_test = d[test==1, ]
+
+set.seed(2)
+res=NDPMix(d_train = d_train, formula = Y ~ X, d_test = d_test, 
+           iter=1000, burnin=500, 
+           phi_y = c(10,10), beta_var_scale = 50, 
+           init_k = 10, mu_scale = 1, tau_scale = .001)
+
+
+png('dp_oscil.png')
+plot(d$X, d$Y, pch=20, 
+     xlim=c(-2,2), ylim=c(-3,3), 
+     col=ifelse(test==1,'pink','lightblue'),
+     axes=F, xlab='X', ylab='Y')
+
+### Plot posterior credible Band 
+colfunc <- colorRampPalette(c("white", "skyblue"))
+ci_perc = seq(.80,.01,-.01)
+colvec = colfunc(length(ci_perc))
+names(colvec) = ci_perc
+
+X_draw = c(d_train$X, d_test$X)
+ord = order(X_draw)
+
+X_draw = X_draw[ord]
+res_stack = rbind(res$predictions$train, res$predictions$test)
+res_stack = res_stack[ord,]
+
+for(i in ci_perc){
+  pci = apply(res_stack, 1, quantile, probs=c( (1-i)/2, (1+i)/2  ) )
+  # polygon(c(X_draw,rev(X_draw)),c(pci[1,],rev(pci[2,])),
+  #         col = colvec[as.character(i)], border = F)
+  segments(X_draw,pci[1,], X_draw, pci[2,], col=colvec[as.character(i)], lwd=4 )
+}
+###
+
+axis(1, -2:2, -2:2)
+axis(2, -3:3, -3:3)
+
+points(d$X, d$Y, pch=20, col=ifelse(test==1,'pink','gray'))
+points(d$X, rowMeans(res_stack), col='steelblue', pch=20, type='o')
+dev.off()
+
+#### ----------------------- BART Model         ----------------------------####
+
+set.seed(3)
+bart_res = bart(x.train = d_train$X, y.train = d_train$Y, x.test = d_test$X)
+
+png('bart_oscil.png')
+plot(d$X, d$Y, pch=20, 
+     xlim=c(-2,2), ylim=c(-3,3), 
+     col=ifelse(test==1,'pink','lightblue'),
+     axes=F, xlab='X', ylab='Y')
+
+### Plot posterior credible Band 
+colfunc <- colorRampPalette(c("white", "skyblue"))
+ci_perc = seq(.99,.01,-.01)
+colvec = colfunc(length(ci_perc))
+names(colvec) = ci_perc
+
+X_draw = c(d_train$X, d_test$X)
+ord = order(X_draw)
+
+X_draw = X_draw[ord]
+res_stack = rbind(t(bart_res$yhat.train), t(bart_res$yhat.test))
+res_stack = res_stack[ord,]
+
+for(i in ci_perc){
+  pci = apply(res_stack, 1, quantile, probs=c( (1-i)/2, (1+i)/2  ) )
+  polygon(c(X_draw,rev(X_draw)),c(pci[1,],rev(pci[2,])),
+          col = colvec[as.character(i)], border = F)
+}
+###
+
+axis(1, -2:2, -2:2)
+axis(2, -3:3, -3:3)
+
+points(d$X, d$Y, pch=20, col=ifelse(test==1,'pink','gray'))
+points(d$X, rowMeans(res_stack), col='steelblue', pch=20, type='l', lwd=1)
+dev.off()
+
+#### ----------------------- Gaussian Process   ----------------------------####
+stan_data <- list(N1=nrow(d_train), 
+                  x1 = d_train$X, y1=d_train$Y,
+                  x2 = d_test$X, N2=length(d_test$X))
+
+## https://mc-stan.org/docs/2_22/stan-users-guide/gaussian-processes-chapter.html
+## GP code from Stan site that implements Guassian Process regression with 
+## inference for hyperparamters.
+## see above link for guidance on hyperprior calibration.
+stan_mod = stan_model("gaussian_process_with_HPs.stan")
+
+## In practice, should run for many more iterations and check convergence diagnostics
+stan_res = sampling(stan_mod, data=stan_data, 
+                    chains=1, iter=100, seed=100)
+
+post_f_test=extract(stan_res, pars=c('f_test'))[[1]]
+post_f_train=extract(stan_res, pars=c('f_train'))[[1]]
+
+png('gp_oscil.png')
+
+plot(d$X, d$Y, pch=20, 
+     xlim=c(-2,2), ylim=c(-3,3), 
+     col=ifelse(test==1,'pink','lightblue'),
+     axes=F, xlab='X', ylab='Y')
+
+### Plot posterior credible Band 
+colfunc <- colorRampPalette(c("white", "skyblue"))
+ci_perc = seq(.99,.01,-.01)
+colvec = colfunc(length(ci_perc))
+names(colvec) = ci_perc
+
+X_draw = c(d_train$X, d_test$X)
+ord = order(X_draw)
+
+X_draw = X_draw[ord]
+res_stack = rbind(t(post_f_train), t(post_f_test))
+res_stack = res_stack[ord,]
+
+for(i in ci_perc){
+  pci = apply(res_stack, 1, quantile, probs=c( (1-i)/2, (1+i)/2  ) )
+  polygon(c(X_draw,rev(X_draw)),c(pci[1,],rev(pci[2,])),
+          col = colvec[as.character(i)], border = F)
+}
+###
+
+axis(1, -2:2, -2:2)
+axis(2, -3:3, -3:3)
+
+points(d$X, d$Y, pch=20, col=ifelse(test==1,'pink','gray'))
+points(d$X, rowMeans(res_stack), col='steelblue', pch=20, type='l', lwd=1)
+dev.off()
